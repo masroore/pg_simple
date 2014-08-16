7660e1ff4bbd1eec371951f80328f783e2f4c68b
Masroor Ehsan <masroore@gmail.com>
2014-08-16 12:54:57 +0600

    added build script

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CWD=$(pwd)
+echo "Generating README.rst"
+pandoc --from=markdown --to=rst --toc --output=README.rst README.md
+
+echo "Building and updloading python egg"
+python setup.py bdist_egg upload
+
+echo "Generating HTML documentation"
+cd docs
+make html
+
+echo "Creating HTML documentation package"
+cd _build/html
+zip -r ${CWD}/pg_simple_doc.zip .
+
+echo "Done"
+cd ${CWD}
