45620b37d1afeb08b74473eea5347619f680351a
Masroor Ehsan <masroore@gmail.com>
2014-08-16 12:53:28 +0600

    version bump

--- pg_simple/__init__.py
+++ pg_simple/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Masroor Ehsan'
 
-VERSION = '0.2'
+VERSION = '0.2.1'
 
 from pool import config_pool, get_pool, SimpleConnectionPool, ThreadedConnectionPool
 from pg_simple import PgSimple
