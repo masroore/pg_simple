0e5ef578a562b779fcc593a1e41d534f1c96eeac
Masroor Ehsan <masroore@gmail.com>
2020-10-05 21:06:53 +0600

    version bump (Py3k compatibility)

--- pg_simple/__init__.py
+++ pg_simple/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Masroor Ehsan'
 
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 
 from .pool import config_pool, SimpleConnectionPool, ThreadedConnectionPool
 from .pg_simple import PgSimple
